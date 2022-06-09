3f59cb0a164fae9ac79bb57298a6bc0274c69e1d
ninggyang <ninggyang@tencent.com>
2022-06-09 14:49:26 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/tdd_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tdd_app.dir/main.cpp.obj"
+  "libtdd_app.dll.a"
+  "tdd_app.exe"
+  "tdd_app.exe.manifest"
+  "tdd_app.pdb"
+  "tdd_app[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tdd_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
